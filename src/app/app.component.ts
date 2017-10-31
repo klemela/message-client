5aa37c0097c3da9bf111f43b8ec46be7fcf30f18
klemela <klemela>
2017-10-31 14:07:49 +0200

    Get messages from the server

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,41 @@
 import { Component } from '@angular/core';
+import {OnInit} from "@angular/core";
+import {Http} from "@angular/http";
+import {Observable} from "rxjs/Observable";
+import 'rxjs/Rx';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   messages = [];
 
+  constructor(private http: Http) {
+  }
+
+  ngOnInit() {
+    this.update();
+  }
+
+  getUrl() {
+    return Observable.of('http://localhost:8080');
+  }
+
+  update() {
+    this.getUrl()
+      .mergeMap(url => this.http.get(url))
+      .subscribe(response => {
+        this.messages = response.json()
+      });
+  }
+
   save(msg) {
-    this.messages.push(msg);
+    // this.messages.push(msg);
+    this.getUrl()
+      .mergeMap(url => this.http.post(url, {message: msg}))
+      .subscribe(() => this.update());
   }
 }
